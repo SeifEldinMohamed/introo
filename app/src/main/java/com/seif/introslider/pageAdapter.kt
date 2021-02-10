9134433c9cad88f8624d1044bb3c11257abeacf0
seif <seifeldinmohamed101@gmail.com>
2021-02-10 22:55:29 +0200

    Design Improvement

--- app/src/main/java/com/seif/introslider/pageAdapter.kt
+++ app/src/main/java/com/seif/introslider/pageAdapter.kt
@@ -6,35 +6,28 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.viewpager.widget.PagerAdapter
 
-class pageAdapter:PagerAdapter {
-
-    lateinit var layouts:IntArray
-    lateinit var inflater:LayoutInflater
-    lateinit var con:Context
-    constructor(layouts:IntArray,con:Context):super(){
-        this.layouts=layouts
-        this.con=con
-        inflater=con.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+class pageAdapter : PagerAdapter {
+     var layouts: IntArray
+     var inflater: LayoutInflater
+     var con: Context
+    constructor(layouts: IntArray, con: Context) : super() {
+        this.layouts = layouts
+        this.con = con
+        inflater = con.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
     }
-
     override fun isViewFromObject(view: View, `object`: Any): Boolean {
-return view==`object`
+        return view == `object`
     }
-
     override fun getCount(): Int {
         return layouts.size
     }
-
     override fun instantiateItem(container: ViewGroup, position: Int): Any {
-var veiw:View=inflater.inflate(layouts[position],container,false)
+        val veiw: View = inflater.inflate(layouts[position], container, false)
         container.addView(veiw)
         return veiw
     }
-
     override fun destroyItem(container: ViewGroup, position: Int, `object`: Any) {
-        var veiw:View=`object` as View
+        val veiw: View = `object` as View
         container.removeView(veiw)
-
     }
-
 }
--- app/src/main/java/com/seif/introslider/prefManager.kt
+++ app/src/main/java/com/seif/introslider/prefManager.kt
@@ -6,33 +6,37 @@ import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 
 class prefManager {
-    lateinit var con:Context
-    lateinit var pref:SharedPreferences
-    constructor(con:Context){
-        this.con=con
+     var con: Context
+    lateinit var pref: SharedPreferences
+
+    constructor(con: Context) {
+        this.con = con
         getsp()
     }
 
     private fun getsp() {
-        pref=con.getSharedPreferences(con.getString(R.string.pref_name),Context.MODE_PRIVATE)
+        pref = con.getSharedPreferences(con.getString(R.string.pref_name), Context.MODE_PRIVATE)
     }
-    fun writesp(){
-        var editor:SharedPreferences.Editor=pref.edit()
-        editor.putString(con.getString(R.string.pref_key),"NEXT")
+
+    fun writesp() {
+        val editor: SharedPreferences.Editor = pref.edit()
+        editor.putString(con.getString(R.string.pref_key), "NEXT")
         editor.commit()
     }
-    fun checkpref():Boolean{
-        var status:Boolean=false
-        if (pref.getString(con.getString(R.string.pref_key),"null").equals("null")){
-            status=false
-        }else{
-            status=true
+
+    fun checkpref(): Boolean {
+        var status: Boolean = false
+        if (pref.getString(con.getString(R.string.pref_key), "null").equals("null")) {
+            status = false
+        } else {
+            status = true
         }
         return status
     }
-    fun clearpref(){
+
+    fun clearpref() {
         pref.edit().clear().commit()
-        con.startActivity(Intent(con,MainActivity::class.java))
+        con.startActivity(Intent(con, MainActivity::class.java))
         (con as AppCompatActivity).finish()
     }
 
